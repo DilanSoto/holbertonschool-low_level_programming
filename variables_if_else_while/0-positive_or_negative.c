1bcaecd22d21b096f834406e476fa821780a302e
DilanSoto <dilanso12@gmail.com>
2023-10-18 10:24:05 -0400

    low level promaming

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdio.h> //esto es inclusion de la biblioteca estandar
+	int main()
+{	
+	int n; //esta es la variable que le asignaremos el valor aleatorio
+
+		if (n > 0) 
+	{
+		printf("%d is positivive/n", n); //si "n" es mayor que 0 imprime que es positivo
+	}
+		else if (n < 0) 
+	{
+		printf("%d is negative/n", n); 
+	}
+		else 
+	{
+		printf("%d is zero/n", n); //si ninguna de las dos anteriores se cumple 'n' debe ser 0
+	} 
+return 0; //indica que el programa termino con exito
+}
