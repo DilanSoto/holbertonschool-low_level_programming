2101d480c15c09ce36a396f7550337e4001faf99
DilanSoto <dilanso12@gmail.com>
2023-10-19 11:27:45 -0400

    c program

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+/**
+*main - Entry Point
+*
+*Return: Always 0 (success)
+*/
+int main()
+{
+char letras;
+
+for (letras = 'a'; letras <= 'z'; letra++)
+{
+	putchar (letra);
+}
+	putchar (/n);
+return (o);
+}
