0e30dec6619bd37909c127416e21b5f1c4a472fd
DilanSoto <dilanso12@gmail.com>
2023-10-19 13:02:17 -0400

    alphabetic

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - Entry Point
+*
+*return: Always 0 (Success)
+*/
+int main (void)
+{
+	char letras;
+	for(letras = 'a'; letras <= 'z'; letras++)
+	{
+	putchar(letras);
+	}
+	for(letras = 'A'; letras <= 'Z'; letras++)
+	{
+	putchar(letras);
+	}
+	putchar('\n');
+return(0);
+}
