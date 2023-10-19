51dacdebbcf36794497dcb1d9f167fa1e2e531d9
DilanSoto <dilanso12@gmail.com>
2023-10-19 14:30:11 -0400

    alphabetic

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main - Entry Point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry Point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	int n;
+
 	for (n = '0'; n <= '9'; n++)
 	{
 		printf("%d", n);
