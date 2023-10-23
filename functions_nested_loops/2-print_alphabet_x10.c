c6016eddae40898069705815b389757d759bc3bd
DilanSoto <dilanso12@gmail.com>
2023-10-22 20:48:14 -0400

    functions

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h" 
+/**
+ * print_alphabet_x10 - print the alphabet 10 times
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char ds;
+	int xtime;
+
+	for (xtime = 0; xtime <= 9; xtime++)
+	{
+		for (ds = 'a'; ds <= 'z'; ds++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
