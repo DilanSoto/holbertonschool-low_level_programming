51244c3fcaddde2f92b7508cc09a22df9afb3135
DilanSoto <dilanso12@gmail.com>
2023-10-23 13:49:03 -0400

    pointers & arrays

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int ds;
+	int ds = 10;
+	*ds = 98;
+		printf("%i/n,*ds)
+	return (o);
+}
