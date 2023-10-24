25124b8875f1a6ad7125a5ba85a4b530067a7486
DilanSoto <dilanso12@gmail.com>
2023-10-24 13:23:50 -0400

    pointers & arrays

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * main - check the code
+ * @s is the pointer
+ * strlen the function i use
+ * Return: the # of char length
+ */
+int _strlen(char *s)
+{
+	int length
+	length = 0;
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return(length)
+}
