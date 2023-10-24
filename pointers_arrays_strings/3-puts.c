7b87ade04edb8e648ab20873ba985dff5032216e
DilanSoto <dilanso12@gmail.com>
2023-10-24 13:54:43 -0400

    pointers & arrays

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ * _puts -print a string followed by a new line .
+ * str: the string to be printed
+ */
+void _puts(char *str)
+{
+	*str != '\o')
+	{
+		putchar (*str);
+		str++;
+	}
+	putchar('\n');
+}
