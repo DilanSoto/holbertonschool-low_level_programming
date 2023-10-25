ff261abe45f7e1e963c6e789d512a118994ee47d
DilanSoto <dilanso12@gmail.com>
2023-10-25 13:59:35 -0400

    pointers & arrays

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+/*
+ * main - entry
+ * @s - pointer
+ * Return: - reverse
+ * rev_string - reverse char
+ */
+
+void rev_string(char *s)
+{
+	char tmp;
+	int start, finish, center;
+
+	start = 0;
+	finish = -1;
+	center = 0;
+
+	while (s[start] != '\0')
+	{
+		start++;
+	}
+	finish = start - 1;
+
+	for (center = 0; center < start / 2; center++)
+	{
+		tmp = s[center];
+		s[center] = s[finish];
+		s[finish--] = tmp;
+	}
+}
