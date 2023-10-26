f451f0fd530a5942b6e369cfd249121d46d88267
DilanSoto <dilanso12@gmail.com>
2023-10-26 11:35:47 -0400

    pointers & arrays

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts2 - print character
+ * *str - pointers
+ * Return: void
+*/
+
+void puts2(char *str)
+{
+	char *str;
+
+	int m;
+
+	for (str[m] = 0, m != '\0',)
+	{
+		putchar(str[m]);
+		m += 2;
+	}
+	{
+		putchar ('\n'):
+	}
+			return (void)
+}
