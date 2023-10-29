766b3a1167d9c45a0f1194127d321f7bac6d2fd2
DilanSoto <dilanso12@gmail.com>
2023-10-29 18:02:02 -0400

    pointers & arrays

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -4,22 +4,33 @@
 /**
  * puts2 - print character
  * @str - pointers
- * Return: void
 */
 
 void puts_half(char *str)
 {
-	int d = 0;
-	int s = 0;
+	int n, len;
 
-	while (str[d] != '\0')
+	len = 0;
+
+	while (str[len] != '\0')
 	{
-d++;
+		len++;
 	}
-	if(d % 2 == 0)
-			putchar(str[d]);
 
-putchar('\n');
-return;
+	if (len % 2)
+	{
+		for (n = (len - 1) / 2; n < len - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
+	}
+	else if (len % 2 == 0)
+	{
+		for (n = len / 2; str[n] != '\0'; n++)
+		{
+			_putchar(str[n]);
+
+		}
+	}
+	_putchar('\n');
 }
-
