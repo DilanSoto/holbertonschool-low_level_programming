44c17295b00ac9a494597e061e1a401e95f4cc0e
DilanSoto <dilanso12@gmail.com>
2023-10-29 20:59:20 -0400

    pointers & arrays

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @s: The string to be capitalized.
+ *
+ * Return: A pointer to the modified string.
+ */
+char *cap_string(char *s)
+{
+	int index = 0;
+	int capitalize_next = 1;
+
+	while (s[index])
+	{
+		if ((s[index] >= 'a' && s[index] <= 'z') && capitalize_next)
+		{
+			s[index] -= 32;
+			capitalize_next = 0;
+		}
+		else if (s[index] == ' ' || s[index] == '\t' ||
+				s[index] == '\n' || s[index] == ',' ||
+				s[index] == ';' || s[index] == '.' ||
+				s[index] == '!' || s[index] == '?' ||
+				s[index] == '"' || s[index] == '(' || s[index] == ')' ||
+				s[index] == '{' || s[index] == '}')
+		{
+			capitalize_next = 1;
+		}
+		else
+		{
+			capitalize_next = 0;
+		}
+		index++;
+	}
+
+	return (s);
+}
