89a986208ba133772060e1b0ebec38ca4aeebbd0
DilanSoto <dilanso12@gmail.com>
2023-12-01 14:38:51 -0400

    single linked list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,1 +1,32 @@
-#include
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list(const list_t *h) print all element of list_T
+ * @h: the first element
+ * Return: the number of nodes
+ * */
+
+//prototipo
+size_t print_list(const list_t *h)
+{
+	size count =0;
+	
+	while (h != NULL)
+	{
+		if(h->str == NULL)
+		{
+			printf([0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u], %s\n", h->len, h->str);
+		}
+	h = h->next;
+	count++;
+	}
+	return count;
+}
+
