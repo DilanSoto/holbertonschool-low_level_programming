fc7d45a51719a2ae2cb573f83201776ab9c03dbd
DilanSoto <dilanso12@gmail.com>
2024-01-18 21:33:37 -0400

    Update search_algos.h

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -5,7 +5,9 @@
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
+
 int binary_search(int *array, size_t size, int value);
+
 void print_array(int *array, size_t min, size_t max);
 
 #endif
